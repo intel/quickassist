1d6b62088739fb5748a509a67b84d1235c812e78
Diego <diego.villalobos@intel.com>
2023-06-30 12:32:05 -0600

    Updates for RN 1.0.50-00003 and QATlib.

---
+++ docs/_downloads/182f441aa51fb5d6fd149e3718a6afb9/qatlistconfig.sh
@@ -0,0 +1,37 @@
+ #!/bin/bash
+
+printf "%-13s | %-5s | %-12s | %-12s | %-10s\n"  "VFIO GROUP" "NODE"   \
+"PF BDF" "VF BDF" "SERVICES"
+echo "--------------------------------------------------------------"
+
+for vfio_group in /dev/vfio/*; do
+    if [ $vfio_group = "/dev/vfio/vfio" ]; then
+        continue
+    fi
+
+    group=${vfio_group##*/}
+    # assume one bdf per iommu group
+    bdf=$(ls /sys/kernel/iommu_groups/$group/devices/)
+    vendor=$(cat /sys/kernel/iommu_groups/$group/devices/$bdf/vendor)
+    node=$(cat /sys/kernel/iommu_groups/$group/devices/$bdf/numa_node)
+    did=$(cat /sys/kernel/iommu_groups/$group/devices/$bdf/device)
+
+    if [ "$vendor" != "0x8086" ]; then
+        continue
+    fi
+
+    if [ "$did" != "0x4941" ] && [ "$did" != "0x4943" ]; then
+        continue
+    fi
+
+    regex='([a-z0-9]+):([a-z0-9]+):.*'
+    [[ $bdf =~ $regex ]]
+    pf_domain=${BASH_REMATCH[1]}
+    pf_bus=${BASH_REMATCH[2]}
+    pf_bdf="$pf_domain:$pf_bus:00.0"
+
+    printf "%-15s %-7s %-14s %-14s %-10s\n"  "$vfio_group"  "$node"    \
+    "$pf_bdf"   "$bdf"                                                 \
+    "$(cat /sys/bus/pci/devices/$pf_bdf/qat/cfg_services)"
+
+done
